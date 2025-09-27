053a4a7c5c227073eb6e10efa78137828b2f41a8
Nazar-Ferents <nazarferents594@gmail.com>
2025-09-27 20:03:05 +0300

    created L1_3 and L1_4

---
+++ class-work/L1/L1_4/L1_4.js
@@ -0,0 +1,20 @@
+console.log('Hello World!');
+
+
+
+
+
+
+
+const PI = 3.14
+console.log(PI);
+// PI = 123;
+
+
+let y = 'bbhvgvhg';
+console.log(y);
+y = 'witcher';
+console.log(y);
+y = 'witcher';
+
+
