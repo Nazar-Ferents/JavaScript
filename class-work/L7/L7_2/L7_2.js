4dfc1078dce89bf4464bead0e20646d752ccd269
Nazar-Ferents <nazarferents594@gmail.com>
2025-11-23 16:19:58 +0200

    completed hw4

--- class-work/L7/L7_2/L7_2.js
+++ class-work/L7/L7_2/L7_2.js
@@ -23,21 +23,23 @@
 // user.greeting = function (msg) {
 //     return `${msg} Hello ${this.name}!`;
 // }
-// console.log(user.greeting());
+// console.log(user.greeting('oh'));
 // let user2 = new User('Anna',20)
-//
 // console.log(user.greeting.apply(user2,['Oh unbelivable']));
+
+// console.log(user.greeting.apply(user2,['Oh unbelivable']));
+
 // console.log(user.greeting.call(user2,'Oh unbelivable'));
 
-function User(name,age){
-    this.name = name;
-    this.age = age;
-}
-
-let user = new User('Jhon',23);
-user.greeting = function (msg) {
-    return `${msg} Hello ${this.name}!`;
-}
-let user2 = new User('Anna',20)
-let greetingCopy = user.greeting.bind(user2,'Oh')
-console.log(greetingCopy());
+// function User(name,age){
+//     this.name = name;
+//     this.age = age;
+// }
+//
+// let user = new User('Jhon',23);
+// user.greeting = function (msg) {
+//     return `${msg} Hello ${this.name}!`;
+// }
+// let user2 = new User('Anna',20)
+// let greetingCopy = user.greeting.bind(user2,'Oh')
+// console.log(greetingCopy());
