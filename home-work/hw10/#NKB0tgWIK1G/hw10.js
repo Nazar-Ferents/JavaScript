1113f4446b78bf19659c65ffe814cd6235e012c8
Nazar-Ferents <nazarferents594@gmail.com>
2026-01-09 14:47:42 +0200

    done hw10

--- home-work/hw10/#NKB0tgWIK1G/hw10.js
+++ home-work/hw10/#NKB0tgWIK1G/hw10.js
@@ -6,8 +6,7 @@ let next = document.getElementById('next');
 
 let array = [];
 
-for (let i = 1; i <= 100; i++){
-
+for (let i =1; i <=100; i++){
     array.push({index:i})
 }
 
@@ -15,36 +14,29 @@ let page = 0;
 const limit = 10;
 
 function render(){
-
     usersDiv.innerHTML = '';
-
-    let start = page  * limit;
+    let start = page * limit;
     let end = start + limit;
-    let slice = array.slice(start, end);
 
+    let slice = array.slice(start, end);
     for (let user of slice){
         let div = document.createElement('div');
-        div.innerText = `${user.index}`;
+        div.innerText = `${user.index}`
         usersDiv.appendChild(div);
     }
 }
 
 next.onclick = function(ev){
-    if ((page+1)*limit < array.length){
+    if((page+1)*limit < array.length){
         page++;
         render();
     }
 }
 
 prev.onclick = function(ev){
-    if (page > 0){
+    if(page > 0){
         page--;
         render();
     }
 }
-
 render();
-
-
-
-
