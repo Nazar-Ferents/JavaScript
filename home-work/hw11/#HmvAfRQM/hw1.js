ec18b4544566f446029bb37723df6a69508344e9
Nazar-Ferents <nazarferents594@gmail.com>
2026-01-14 00:39:04 +0200

    done hw1

--- home-work/hw11/#HmvAfRQM/hw1.js
+++ home-work/hw11/#HmvAfRQM/hw1.js
@@ -2,51 +2,65 @@
 
 fetch('https://dummyjson.com/carts')
     .then(res => res.json())
-    .then(data => {
-        let carts_container = document.createElement('div');
-        carts_container.classList.add('carts_container');
-        document.body.appendChild(carts_container);
-        for (let cart of data.carts) {
-            let cartDiv = document.createElement('div');
-            cartDiv.classList.add('cart_div');
-            carts_container.appendChild(cartDiv);
-            for(let key in cart) {
-                if (typeof cart[key] !== 'object') {
-                    let p = document.createElement('p');
-                    p.innerText = `${key}: ${cart[key]}`;
-                    cartDiv.appendChild(p)
-                }
-                else if (typeof cart[key] === 'object') {
-                    let divInfo = document.createElement('div');
-                    let ol = document.createElement('ol');
-                    let pObj = document.createElement('p');
-                    pObj.innerText = `${key}:`
-                    for (let obj of cart[key]) {
-                        for (let objKey in obj) {
-                            let li = document.createElement('li');
-                            li.innerText = `${objKey}:${obj[objKey]}`;
+    .then(cartsObject => {
+        let {carts} = cartsObject;
+        console.log(carts);
 
-                            ol.appendChild(li);
-                            divInfo.append(pObj,ol)
+        let mainDiv = document.createElement('div');
+        mainDiv.classList.add('main_div');
+        document.body.appendChild(mainDiv);
+
+        for (let cart of carts) {
+            let cart_div = document.createElement('div');
+            cart_div.classList.add('cart_div');
+            let infoDiv = document.createElement('div');
+            infoDiv.classList.add('info_div');
+            infoDiv.innerText =
+                `
+                discountedTotal: ${cart.discountedTotal}    
+                id: ${cart.id}    
+                total: ${cart.total}   
+                totalProducts: ${cart.totalProducts}    
+                totalQuantity: ${cart.totalQuantity}    
+                userId: ${cart.userId}
+            `;
+
+            let ol = document.createElement('ol');
 
 
-                        }
+            for (let product of cart.products) {
+
+                const li = document.createElement('li');
+                const info = document.createElement('p');
+                info.innerText =
+                    `
+                    "id": ${product.id},
+                      "title": ${product.title},
+                      "price": ${product.price},
+                      "quantity": ${product.quantity},
+                      "total": ${product.total},
+                      "discountPercentage": ${product.discountPercentage},
+                      "discountedTotal": ${product.discountedTotal},
+                `
+                let img = document.createElement('img');
+                img.src = product.thumbnail;
+                li.append(img,info);
+                ol.append(li);
 
 
-
-
-                    }
-                    cartDiv.append(divInfo);
-
-                }
 
             }
 
+            cart_div.append(infoDiv,ol)
+            mainDiv.appendChild(cart_div)
+
+
+
+
+
         }
 
 
-console.log(data);
-        }
+        });
 
-    );
 
