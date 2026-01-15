f049dbf006308f6bac133dbbeae0e7f6434d2a3e
Nazar-Ferents <nazarferents594@gmail.com>
2026-01-15 19:16:54 +0200

    change L12_7

--- class-work/L12/L12_7/L12_7.js
+++ class-work/L12/L12_7/L12_7.js
@@ -12,8 +12,29 @@
 //         console.log(user);
 //     }
 // })
+
+// let users = fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(res => res.json())
+// let posts = fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then(res => res.json())
+// Promise.all([users,posts]).then((result)=>{
+//     let [users, posts] = result;
+//
+//     for(const user of users){
+//         user.posts =[];
+//
+//         for(const post of posts){
+//             if(user.id === post.userId){
+//                 user.posts.push(post);
+//             }
+//         }
+//     }
+//     console.log(users);
+//
+// })
+
 let users = fetch("https://jsonplaceholder.typicode.com/users")
     .then(res => res.json())
-let posts = fetch("https://jsonplaceholder.typicode.com/posts?userId="+user.id)
+let posts = fetch("https://jsonplaceholder.typicode.com/posts")
     .then(res => res.json())
-Promise.all([x,y])
+Promise.any([users,posts]).then((value)=>{console.log(value)});
